c39577bef183f4841c81b10b6779911a895b43f1
Damian Zawidzki <pepik130690@gmail.com>
2025-11-08 00:01:37 +0000

    main activity

---
+++ app/src/main/java/com/example/dzcoffee/SuperAdminActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dzcoffee
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SuperAdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_superadmin)
+    }
+}
